b699ddb17b677d7c4cf15f4eeb340ab3d44a8d00
Gbriel70 <bielcostamatos@gmai.com>
2026-09-02 10:05:27 -0300

    nginx alpine startup and healtcheck

--- docker_srcs/nginx/entrypoint.sh
+++ docker_srcs/nginx/entrypoint.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-
-# Script customizado que copia configs após a imagem base inicializar
-# Permite evitar conflitos de permissão
-
-# Aguarda a imagem base preparar os diretórios
-sleep 2
-
-# Copia o modsecurity.conf customizado (sobrescreve o da imagem se necessário)
-# A imagem gera um nginx.conf que inclui conf.d/ automaticamente
-# e conf.d/custom.conf será carregado
-
-echo "🔧 Configurando Nginx + ModSecurity..."
 
 # Executa o entrypoint padrão do docker-entrypoint.sh
 exec /docker-entrypoint.sh "$@"
